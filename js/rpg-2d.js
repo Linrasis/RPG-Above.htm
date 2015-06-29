a1c055f1394b0e20e7a0891443d7c13a998ed49b
Jan Horava <czechinthemail@gmail.com>
2015-06-28 18:52:30 -0700

    added cast spell to list of actions

--- js/rpg-2d.js
+++ js/rpg-2d.js
@@ -468,7 +468,7 @@ function setmode(newmode, newgame){
     buffer = 0;
     canvas = 0;
 
-    document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick="setmode(1, true)">Test Level</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled style=border:0 value=ESC>Main Menu<br><input id=movement-keys maxlength=4 value='
+    document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick="setmode(1, true)">Test Level</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled style=border:0 value=Click>Cast Spell<br><input disabled style=border:0 value=ESC>Main Menu<br><input id=movement-keys maxlength=4 value='
       + settings['movement-keys'] + '>Move ↑←↓→</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
       + settings['audio-volume'] + '>Audio<br><input id=color type=color value='
       + settings['color'] + '>Color<br><input id=ms-per-frame value='
