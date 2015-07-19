8751abe102838e1eb74a419121fdd961586df9f8
Jan Horava <czechinthemail@gmail.com>
2015-07-19 09:55:00 -0700

    moved level stuff into js/levels.js

--- js/rpg-above.js
+++ js/rpg-above.js
@@ -659,64 +659,7 @@ function setmode(newmode, newgame){
             resize();
         }
 
-        create_world_dynamic({
-          'color': '#222',
-          'height': 100,
-          'width': 25,
-          'x': -150,
-          'y': -150,
-        });
-        create_world_dynamic({
-          'collision': false,
-          'color': '#700',
-          'effect': 1,
-          'height': 50,
-          'width': 50,
-          'x': 150,
-          'y': 150,
-        });
-        create_world_dynamic({
-          'collision': false,
-          'color': '#66f',
-          'effect': 1,
-          'effect-stat': 'mana',
-          'height': 50,
-          'width': 50,
-          'x': 100,
-          'y': 150,
-        });
-        world_static.push({
-          'color': '#333',
-          'height': 500,
-          'width': 500,
-          'x': -250,
-          'y': -250,
-        });
-
-        create_npc({
-          'friendly': true,
-          'x': -200,
-          'y': 100,
-        });
-        create_npc({
-          'selected': 'bolt',
-          'spellbook': {
-            'bolt': {
-              'current': 0,
-              'damage': 1,
-              'lifespan': 50,
-              'reload': 10,
-            },
-          },
-          'stats': {
-            'health': {
-              'current': 10,
-              'max': 10,
-            },
-          },
-          'x': 200,
-          'y': -100,
-        });
+        load_level(0);
 
         animationFrame = window.requestAnimationFrame(draw);
         interval = window.setInterval(
